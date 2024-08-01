8d9a6f826a24b7a1ebd9ee196a841055d663db32
divyaanjali301 <divyaanjali58@gmail.com>
2024-08-01 10:36:38 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 class demo
 {
   public static void main(String[] args) {
-    System.out.println("welcome to Git");
+    System.out.println("Hii this is first git class for Arun and Anjali");
   }
 }
